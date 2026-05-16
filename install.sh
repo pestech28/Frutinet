bf80caa3336b69eac32efd192d955a344e26e60b
pestech28 <pestech28@yahoo.com>
2026-05-15 23:42:28 -0400

    Updated for dir change and extracting iStart

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ sudo apt update
 sudo apt install tinyproxy -y
 sudo apt install apache2 -y
 sudo ufw allow 8888/tcp
-sudo cp tinyproxy.conf /etc/tinyproxy -f
-sudo cp filter /etc/tinyproxy -f
+sudo cp tinyproxy/tinyproxy.conf /etc/tinyproxy -f
+sudo cp tinyproxy/filter /etc/tinyproxy -f
 sudo cp sites/istart.conf /etc/apache2/sites-available/ -f
 sudo a2ensite istart.conf
 echo "127.0.0.1 istart.net" | sudo tee -a /etc/hosts
+tar -xf /www/istart.tar --overwrite -C /var/www/html
 sudo systemctl reload apache2
 sudo systemctl restart tinyproxy
