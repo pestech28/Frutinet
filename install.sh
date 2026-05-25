181c40cd54ac028a672d22189e43666ee6e8e112
pestech28 <pestech28@yahoo.com>
2026-05-25 13:27:08 -0400

    Added code for extracting front
    Signed-off-by: pestech28 <pestech28@yahoo.com>

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo cp tinyproxy/filter /etc/tinyproxy -f
 sudo cp sites/istart.conf /etc/apache2/sites-available/ -f
 sudo a2ensite istart.conf
 echo "127.0.0.1 istart.net" | sudo tee -a /etc/hosts
-tar -xf www/istart.tar --overwrite -C /var/www/html
+tar -xf www/front.tar --overwrite -C /var/www/html
+tar -xf www/istart.tar --overwrite -C /var/www/html/istart
 sudo systemctl reload apache2
 sudo systemctl restart tinyproxy
