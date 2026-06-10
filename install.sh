b18bf31375ca5b0a3e0d07ac6b23a04cfa1b7aee
pestech28 <pestech28@yahoo.com>
2026-06-09 20:07:38 -0400

    Added apple.com
    Signed-off-by: pestech28 <pestech28@yahoo.com>

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ sudo apt install apache2 -y
 sudo ufw allow 8888/tcp
 sudo cp tinyproxy/tinyproxy.conf /etc/tinyproxy -f
 sudo cp tinyproxy/filter /etc/tinyproxy -f
-sudo tar -xfv www/front.tar --overwrite -C /var/www/html
+sudo tar -xvf www/front.tar --overwrite -C /var/www/html
 sudo cp sites/istart.conf /etc/apache2/sites-available/ -f
 sudo a2ensite istart.conf
 echo "127.0.0.1 istart.net" | sudo tee -a /etc/hosts
-sudo tar -xfv www/istart.tar --overwrite -C /var/www/html/istart
+sudo tar -xvf www/istart.tar --overwrite -C /var/www/html/istart
 sudo cp sites/nintendo.conf /etc/apache2/sites-available/ -f
 sudo a2ensite nintendo.conf
 echo "127.0.0.1 nintendo.com" | sudo tee -a /etc/hosts
 sudo mkdir /var/www/html/restore/nintendo/
-sudo tar -xfv www/restore/nintendo.tar --overwrite -C /var/www/html/restore/nintendo
+sudo tar -xvf www/restore/nintendo.tar --overwrite -C /var/www/html/restore/nintendo
 sudo cp sites/apple.conf /etc/apache2/sites-available/ -f
 sudo a2ensite apple.conf
 echo "127.0.0.1 apple.com" | sudo tee -a /etc/hosts
