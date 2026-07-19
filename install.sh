104578634d2046333af181615342d69a4cc73ee9
pestech28 <pestech28@yahoo.com>
2026-07-19 10:30:47 -0400

    added make folder code
    Signed-off-by: pestech28 <pestech28@yahoo.com>

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ sudo ufw allow 8888/tcp
 sudo cp tinyproxy/tinyproxy.conf /etc/tinyproxy -f
 sudo cp tinyproxy/filter /etc/tinyproxy -f
 sudo tar -xvf www/front.tar --overwrite -C /var/www/html
+sudo mkdir /var/www/html/restore/istart/
+sudo mkdir /var/www/html/restore/restore/
+sudo mkdir /var/www/html/restore/live/
 sudo cp sites/istart.conf /etc/apache2/sites-available/ -f
 sudo a2ensite istart.conf
 echo "127.0.0.1 istart.net" | sudo tee -a /etc/hosts
